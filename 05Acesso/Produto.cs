b7b8b6eb8413587853957f8356dbe2ecd1749ea0
0left <leftamail@gmail.com>
2022-02-14 21:55:02 -0300

    Aula05 Fim
    Modificadores de acesso

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,14 @@
+class Produto
+{
+    /* Modificadores
+        Public      --> ATR e MET Visiveis em qualquer classe
+        Private     --> ATR e MET Visiveis apenas na classe onde são criados
+        Protected   --> ATR e MET Visiveis em classes onde são criados ou herdados
+    
+        internal -> C#
+        default -> JAVA
+    */
+
+    public string nome;
+    private double valor;
+}
