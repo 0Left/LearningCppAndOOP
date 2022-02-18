761892bdb30d8ed546200847221ffa50bc19de26
0left <leftamail@gmail.com>
2022-02-17 21:00:50 -0300

    Assunto 10 Herança

---
+++ 10Heranca/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Pessoa p = new Pessoa("Left");
+            Colaborador c = new Colaborador("Left",22,2000);
+        }
+    }
+}
