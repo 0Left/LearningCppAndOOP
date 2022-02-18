ffb710d8865c3d9fc863c9d2068beaf1523f0785
0left <leftamail@gmail.com>
2022-02-17 22:28:56 -0300

    Assunto 14 Interface
    Finalizei, e de quebra ainda aprendi como usa aquele string[] args na Main()

---
+++ 14Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _14Interface
+{
+    class Program{
+
+        static void Main(string[] args){
+            Calculo c = new Calculo();
+            c.somar(1,10);
+            c.subtrair(10,1);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("arg:" + args[i]);
+                
+            }
+        }
+    }
+
+}
