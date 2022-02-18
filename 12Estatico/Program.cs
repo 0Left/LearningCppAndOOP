02b45fe6d8a11ee1839bf176139707aabbd2387c
0left <leftamail@gmail.com>
2022-02-17 21:47:59 -0300

    Assunto 12  Static

---
+++ 12Estatico/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Exemplo.soma(20,20);
+            Console.WriteLine(Exemplo.iD);
+            Exemplo.iD = "Fuen";
+            Console.WriteLine(Exemplo.iD);
+            Exemplo Obj = new Exemplo();
+            Console.WriteLine(Obj.name);
+            
+        }
+    }
+}
