ef61d37074026ed95bc60e5821d11c35598a4233
0left <leftamail@gmail.com>
2022-02-17 22:04:03 -0300

    Assunto 13 Abstração
    Acho que agora eu entendi em

---
+++ 13Abstracao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            PF pf = new PF();
+            pf.taxaEmprestimo(1000);
+
+            PJ pj = new PJ();
+            pj.taxaEmprestimo(1000);
+        }
+    }
+}
